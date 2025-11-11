284950f94e946776f3834f8b34aae91a0a2bce8a
Gage W. <64271462+Nokoia@users.noreply.github.com>
2025-11-11 12:26:08 -0500

    Updated: Stored Procedures Complete +1 FN

--- Stored Procedures and Functions/Stored Procedures and Functions.sql
+++ Stored Procedures and Functions/Stored Procedures and Functions.sql
@@ -118,6 +118,91 @@ begin
 end//
 delimiter ; 
 
-
-
+/*
+Allows submission of rating
+*/
+delimiter //
+create procedure SP_Rating_Rate
+(
+	IN resource_ID int unsigned,
+	IN rater varchar(50),
+    IN rating numeric(2,1),
+    IN date_of date
+)
+begin
+	insert into rating
+		(
+			ResourceID,
+			Poster,
+            Score,
+            Date
+		)
+	values
+    (
+		resource_ID,
+		rater,
+        rating,
+        date_of
+	);
+    update Resource
+		set Rating =
+        (
+        Select avg(r.Score) 
+        from Rating as r
+        where r.ResourceID = resource_ID
+        )
+	where ResourceID = resource_ID;
     
+    
+end//
+delimiter ;
+
+/*
+Gets details for a resource based on ResourceID
+*/
+delimiter //
+create procedure SP_Resource_Details
+(
+	IN resource_ID int unsigned
+)
+begin
+select
+	R.ResourceID, 
+    R.Topic, 
+    R.Format, 
+    R.DateFor,
+    R.Author as Author_Name,
+    N.Body as Note_Body,
+    W.Link as Web_Address,
+    V.Duration as Video_Duration,
+    
+    (
+    select avg(score) 
+    from Rating 
+    where ResourceID = R.ResourceID
+    ) as Average_Rating
+    
+from Resource as R join user as U on R.Author = U.Name
+left join Note as N on R.ResourceID = N.ResourceID
+left join Website as W on R.ResourceID = W.ResourceID
+left join Video as V on R.ResourceID = V.ResourceID
+
+where R.ResourceID = resource_ID;
+
+end//
+delimiter ; 
+
+/*
+Returns average score of given ResourceID
+*/
+delimiter //
+create function FN_Rating_Avg(resource_id int)
+	returns decimal(2,1)
+	begin
+	declare r_avg decimal(2,1);
+		select avg(Rating) into r_avg
+		from Resource
+		where Resource.ResourceID = resource_id;
+	return r_avg;
+end
+
