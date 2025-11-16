447b15f154dc7631bde3600b54c7884034e5743c
Jove Wright Emmons <144271740+JoveEUSM@users.noreply.github.com>
2025-11-15 22:19:57 -0500

    Made changes to import data script to leverage the SP_Resource_Create procedure, and to ensure proper functionality.

---
+++ Nikki - Jove Lobster Notes Import Data.sql
@@ -0,0 +1,185 @@
+/*
+Lobster Notes Import Data
+Gabrielle Akers
+November 12, 2025
+*/
+create table StageWebData(
+	DataID int unsigned auto_increment primary key,
+    WebData JSON not null,
+    Imported int default 0
+);
+
+-- Import data scraped from Khan Academy
+delimiter //
+create procedure ImportData(in did int)
+this_proc:begin
+    -- Load JSON
+    declare j JSON;
+    set j = null;
+    
+    select WebData into j, count() from StageWebData where DataID = did and Imported = 0;
+
+    if j is null then
+        leave this_proc;
+    end if;
+
+
+
+    -- Ensure canonical fallback author exists so FK on Resource.Author won't fail
+    insert ignore into User(Name) values('Web Scraped',"",True);
+
+    declare resourceCount int unsigned;
+    declare noteCount int unsigned;
+    declare WebsiteCount int unsigned;
+    declare pdfcount int unsigned;
+    declare imagecount int unsigned;
+    declare videocount int unsigned;
+
+    declare lecture_date date; -- done
+    declare author_of varchar(50); -- done
+    declare lecture_topic varchar(25); -- done
+    declare resource_keywords varchar(25); -- done
+    declare format_of varchar(7); -- done
+    declare note_body varchar(2048);
+    declare web_address varchar(2048);
+    declare video_duration int unsigned;
+    declare image_size int unsigned;
+
+    set resourceCount = 0;
+    set noteCount = 0;
+    set websiteCount = 0;
+    set pdfCount = 0;
+    set imageCount = 0;
+    set videoCount = 0;
+    /*
+        -while loop over resource array
+        -get the info we can from the resource entry
+            -check the format value
+        -based on the value, check the format arrays
+        -get the values we need from that
+        -call sp_resource_create with those values, others as dummy values - still within the formats case
+    repeat
+
+
+    need a counter of formats so we can tell which one to use from array
+    start at 0, go up by 1 each time we get a format inst
+    use that with for ordinality column in json_table to get the right one
+
+    */
+
+
+    set resource_keywords = null;
+
+    while resourcecount < json_length(j,'$.Resource') do
+        set resourcecount = resourcecount + 1;
+
+        select COALESCE(rjt.rdatefor,CURDATE()) into lecture_date,
+        COALESCE(rjt.rauthor, 'Web Scraped'), into author_of,
+        COALESCE(rjt.rtopic, 'n/a') into lecture_topic,
+        rjt.rFormat into format_of
+        from (json_table(
+            cast(j as json),
+            '$.Resource[*]' COLUMNS(
+                count for ordinality,
+                Rdatefor date PATH '$.DateFor',
+                rauthor varchar(50) path '$.Author',
+                rtopic varchar(25) path '$.Topic',
+                rFormat varchar(7) path '$.Format'
+                )
+        )) as RJT
+        where rjt.count = resourcecount;
+        
+
+
+        case format_of
+
+            when 'Note' then -- needs body
+                set noteCount = notecount + 1;
+                select njt.nbody into note_body
+                from (json_table(
+                    cast(j as json),
+                    '$.Note[*]' COLUMNS(
+                        count for ordinality,
+                        nbody varchar(2048) path '$.Body'
+                    )
+                )) as njt
+                where njt.count = notecount;
+                call SP_Resource_Create(lecture_date,author_of,lecture_topic,
+                    resource_keywords,format_of,note_body,"",
+                    0,0);
+
+            when 'Website' then -- needs link
+                set websiteCount = websitecount + 1;
+                select wjt.wlink into web_address
+                from (json_table(
+                    cast(j as json),
+                    '$.Website[*]' COLUMNS(
+                        count for ordinality,
+                        wlink varchar(2048) path '$.Link'
+                    )
+                )) as wjt
+                where wjt.count = websitecount;
+                call SP_Resource_Create(lecture_date,author_of,lecture_topic,
+                    resource_keywords,format_of,"",web_address,
+                    0,0);
+
+            when 'pdf' then -- needs body, link
+                set pdfCount = pdfcount + 1;
+                select pjt.pbody into note_body, pjt.plink into web_address
+                from (json_table(
+                    cast(j as json),
+                    '$.pdf[*]' COLUMNS(
+                        count for ordinality,
+                        pbody varchar(2048) path '$.Body',
+                        plink varchar(2048) path '$.Link'
+                    )
+                )) as pjt
+                where pjt.count = pdfcount;
+                call SP_Resource_Create(lecture_date,author_of,lecture_topic,
+                    resource_keywords,format_of,note_body,web_address,
+                    0,0);
+
+            when 'Image' then -- needs size, link
+                set imageCount = imagecount + 1;
+                select ijt.isize into image_size, ijt.ilink into web_address
+                from (json_table(
+                    cast(j as json),
+                    '$.Image[*]' COLUMNS(
+                        count for ordinality,
+                        isize int unsigned path '$.Size',
+                        ilink varchar(2048) path '$.Link'
+                    )
+                )) as ijt
+                where ijt.count = imagecount;
+                call SP_Resource_Create(lecture_date,author_of,lecture_topic,
+                    resource_keywords,format_of,"",web_address,
+                    0,image_size);
+
+            when 'Video' then -- needs duration, link
+                set videoCount = videocount + 1;
+                select vjt.vduration into video_duration, vjt.vlink into web_address
+                from (json_table(
+                    cast(j as json),
+                    '$.Video[*]' COLUMNS(
+                        count for ordinality,
+                        vlink varchar(2048) path '$.Link',
+                        vduration int unsigned path '$.Duration'
+                    )
+                )) as vjt
+                where vjt.count = videocount;
+                call SP_Resource_Create(lecture_date,author_of,lecture_topic,
+                    resource_keywords,format_of,"",web_address,
+                    video_duration,0);
+
+        end case;
+        
+
+    end while;
+
+    update StageWebData set Imported = 1 where DataID = did;
+
+
+
+
+end//
+delimiter ;
