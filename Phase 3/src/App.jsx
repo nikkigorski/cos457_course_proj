4ed969015a8894a4980d5212d3d98c1f806ffac0
Gabrielle Akers <144271532+GAkers2002@users.noreply.github.com>
2025-12-01 15:57:29 -0500

    Create App.jsx

---
+++ Phase 3/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AccountCreation from "./AccountCreation.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <AccountCreation />
+    </div>
+  );
+}
