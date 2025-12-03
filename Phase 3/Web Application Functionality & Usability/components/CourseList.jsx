58a405578fd1aff68fdd5c8b10a999849bef606b
Gage W. <64271462+Nokoia@users.noreply.github.com>
2025-12-02 20:01:57 -0500

    Add files via upload
    Updated files to work with larger project

--- Phase 3/Web Application Functionality & Usability/components/CourseList.jsx
+++ Phase 3/Web Application Functionality & Usability/components/CourseList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
-function CourseList({ courses }) {
+function CourseList({ courses, onCourseSelect }) {
   return (
     <div className="card shadow-sm">
       <div className="card-header bg-primary text-white">
@@ -12,23 +12,23 @@ function CourseList({ courses }) {
           // Standard function syntax
           return (
 
-            <li key={course.CourseID} className="list-group-item p-0">
-              
-            <Link 
-              to={`/course/${course.CourseID}`} 
-              key={course.CourseID} 
-              className="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3"
-              style={{textDecoration: 'none', color: 'inherit'}}
+            <li key={course.CourseID} 
+            className="list-group-item p-0"
             >
-              <div>
-                {/* Combines subject and catalog number into a course */}
-                <strong>{course.Subject} {course.CatalogNumber}</strong>: {course.Name}
-                <br/>
-                {/* Display Session, Year and Section */}
-                <small className="text-muted">
-                  {course.Session} {course.Year} | Section {course.Section}
-                </small>
-              </div>
+              
+            <div 
+                className="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3"
+                style={{textDecoration: 'none', color: 'inherit', cursor: 'pointer'}}
+             
+                onClick={() => onCourseSelect(course.CourseID)} 
+              >
+                <div>
+                  <strong>{course.Subject} {course.CatalogNumber}</strong>: {course.Name}
+                  <br/>
+                  <small className="text-muted">
+                    {course.Session} {course.Year} | Section {course.Section}
+                  </small>
+                </div>
               
               <div className="text-end">
                 {/* ProfessorID for course */}
@@ -36,7 +36,7 @@ function CourseList({ courses }) {
                   Prof ID: {course.ProfessorID}
                 </span>
               </div>
-              </Link>
+              </div>
             </li>
           );
         })}
