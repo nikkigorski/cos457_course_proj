a394418cfcffbd562d0d8c99f30496954e06ef36
Gabrielle Akers <144271532+GAkers2002@users.noreply.github.com>
2025-12-08 17:35:10 -0500

    Adds course selection screens

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   root: process.cwd(),
   server: {
     port: 5173,
-    host: 'localhost'
+    host: "localhost"
+  },
+
+  // load account creation
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        professor: resolve(__dirname, "professor.html"),
+        student: resolve(__dirname, "student.html"),
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   }
-})
+});
